f06e4c81b17c40616d9046d7c0ce0f8deb3d4fb3
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-06-22 00:05:41 +0200

    Cleanup controller.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,79 +2,61 @@ class ExpensesController < ApplicationController
   before_action :set_expense, only: [:show, :edit, :update, :destroy]
   before_action :set_date, only: :index
 
-  # GET /expenses
-  # GET /expenses.json
   def index
     @expenses = Expense.by_quarter @filter_date
   end
 
-  # GET /expenses/1
-  # GET /expenses/1.json
   def show
   end
 
-  # GET /expenses/new
   def new
     @expense = Expense.new
   end
 
-  # GET /expenses/1/edit
   def edit
   end
 
-  # POST /expenses
-  # POST /expenses.json
   def create
     @expense = Expense.new(expense_params)
 
     respond_to do |format|
       if @expense.save
         format.html { redirect_to @expense, notice: 'Expense was successfully created.' }
-        format.json { render :show, status: :created, location: @expense }
       else
         format.html { render :new }
-        format.json { render json: @expense.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /expenses/1
-  # PATCH/PUT /expenses/1.json
   def update
     respond_to do |format|
       if @expense.update(expense_params)
         format.html { redirect_to @expense, notice: 'Expense was successfully updated.' }
-        format.json { render :show, status: :ok, location: @expense }
       else
         format.html { render :edit }
-        format.json { render json: @expense.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /expenses/1
-  # DELETE /expenses/1.json
   def destroy
     @expense.destroy
     respond_to do |format|
       format.html { redirect_to expenses_url, notice: 'Expense was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
+
   def set_date
     @filter_date = Date.parse params[:date]
   rescue
     @filter_date = Date.today if @filter_date.nil?
   end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_expense
     @expense = Expense.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def expense_params
     params.require(:expense).permit(:issued_at, :description, :total_price, :tax_price)
   end
