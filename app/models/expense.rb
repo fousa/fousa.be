28466850381f7766b98e02837f5359c685a58a77
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-06-21 23:31:25 +0200

    Show if expense is incomplete in the overview.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,8 @@
 class Expense < ActiveRecord::Base
   validates :issued_at, presence: true
   validates :description, presence: true
+
+  def complete?
+    total_price.present? && tax_price.present?
+  end
 end
