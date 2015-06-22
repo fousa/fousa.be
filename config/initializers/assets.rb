7fa884138070b98650cb95ab4dc128b877340154
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-06-22 14:47:33 +0200

    Move fonts.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
+Rails.application.config.assets.paths << "#{Rails.root}/app/assets/fonts"
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 
