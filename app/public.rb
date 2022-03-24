13ca7675a7c6ded6a054b519a10d369715c372b7
Jelle Vandebeeck <jelle@fousa.be>
2022-03-24 20:03:17 +0100

    Format the public file.

--- app/public.rb
+++ app/public.rb
@@ -4,67 +4,67 @@ require 'sinatra/base'
 require './app/helpers/struct'
 
 module Vulture
-    module Public
-        def self.registered app
-            app.get '/' do
-                config = YAML.load_file 'config/app/index.yml'
-                
-                # Prepare the texts.
-                text = config['text'].to_struct
-                @developer_text = text.developer
-                @gliding_text = text.gliding
-                
-                # Prepare the apps.
-                @apps = config['apps'].map { |app| app.to_struct }
+  module Public
+    def self.registered(app)
+      app.get '/' do
+        config = YAML.load_file 'config/app/index.yml'
 
-                haml :index
-            end
+        # Prepare the texts.
+        text = config['text'].to_struct
+        @developer_text = text.developer
+        @gliding_text = text.gliding
 
-            app.get '/projects' do
-                config = YAML.load_file 'config/app/projects.yml'
+        # Prepare the apps.
+        @apps = config['apps'].map(&to_struct)
 
-                #Prepare the projects
-                @technologies = config['technologies'].map do |technology| 
-                    struct = technology.to_struct
-                    struct.projects = struct.projects.map do |project|
-                        project.to_struct
-                    end
-                    struct
-                end
+        haml :index
+      end
 
-                # Count the projects
-                @project_count = @technologies.inject(0) do |count, technology|
-                    count += technology.projects.count
-                end
+      app.get '/projects' do
+        config = YAML.load_file 'config/app/projects.yml'
 
-                haml :projects
-            end
-
-            app.get '/projects/:id' do
-                config = YAML.load_file 'config/app/projects.yml'
-
-                # Find the project
-                projects = config['technologies'].inject(Array.new) do |projects, technology|
-                    projects + technology['projects']
-                end
-                project = projects.detect { |p| p['id'] == params[:id] }
-                raise 'Project not found' if project.nil?
-
-                @project = project.to_struct
-
-                haml :project
-            end
-
-            app.get '/apps/:id' do
-                config = YAML.load_file 'config/app/apps.yml'
-
-                app = config['apps'].detect { |p| p['id'] == params[:id] }
-                raise 'App not found' if app.nil?
-
-                @app = app.to_struct
-
-                haml :app
-            end
+        # Prepare the projects
+        @technologies = config['technologies'].map do |technology|
+          struct = technology.to_struct
+          struct.projects = struct.projects.map do |project|
+            project.to_struct
+          end
+          struct
         end
+
+        # Count the projects
+        @project_count = @technologies.inject(0) do |count, technology|
+          count + technology.projects.count
+        end
+
+        haml :projects
+      end
+
+      app.get '/projects/:id' do
+        config = YAML.load_file 'config/app/projects.yml'
+
+        # Find the project
+        projects = config['technologies'].inject([]) do |projects, technology|
+          projects + technology['projects']
+        end
+        project = projects.detect { |p| p['id'] == params[:id] }
+        raise 'Project not found' if project.nil?
+
+        @project = project.to_struct
+
+        haml :project
+      end
+
+      app.get '/apps/:id' do
+        config = YAML.load_file 'config/app/apps.yml'
+
+        app = config['apps'].detect { |p| p['id'] == params[:id] }
+        raise 'App not found' if app.nil?
+
+        @app = app.to_struct
+
+        haml :app
+      end
     end
+  end
 end
