2b30fb5602686e2828709c985f45b6a81a8fa365
Jelle Vandebeeck <jelle@fousa.be>
2022-03-24 20:01:57 +0100

    Handle 404 correctly.

--- app.rb
+++ app.rb
@@ -5,19 +5,19 @@ require 'sinatra/content_for'
 require './app/public'
 
 class App < Sinatra::Base
-    helpers Sinatra::ContentFor
-    register Sinatra::Namespace
+  helpers Sinatra::ContentFor
+  register Sinatra::Namespace
 
-    # Register the public modules.
-    register Vulture::Public
+  # Register the public modules.
+  register Vulture::Public
 
-    configure :development do
-        # Enable auto reload in development.
-        register Sinatra::Reloader
-    end
+  configure :development do
+    # Enable auto reload in development.
+    register Sinatra::Reloader
+  end
 
-    # Handle unfound pages.
-    not_found do
-        haml :'404', :layout => :'layouts/simple'
-    end
+  # Handle unfound pages.
+  not_found do
+    haml :'404'
+  end
 end
